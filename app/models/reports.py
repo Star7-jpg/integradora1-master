9cbc3d7f8d5c31dc7dd5cd80009d0c64c675d4c9
Star7-jpg <marcosconde612@gmail.com>
2023-08-02 19:39:47 -0600

    03 Seccion Reportes

---
+++ app/models/reports.py
@@ -0,0 +1,44 @@
+from .db import get_connection
+
+mydb = get_connection()
+
+class Reports:
+
+    def __init__(self, fecha, total, id_producto, id_usuario, id_venta=None):
+        self.id_venta = id_venta
+        self.fecha = fecha
+        self.total = total
+        self.id_producto = id_producto
+        self.id_usuario = id_usuario
+        
+    @staticmethod
+    def get(id_venta):
+        with mydb.cursor(dictionary=True) as cursor:
+            sql = f"SELECT fecha, total, id_producto, id_usuario  FROM sale WHERE id_venta = { id_venta }"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
+            fecha = Reports(result["fecha"], result["total"], result["id_producto"], result["id_usuario"], id_venta)
+            return fecha
+        
+    @staticmethod
+    def get_all():
+        sale = []
+        with mydb.cursor(dictionary=True) as cursor:
+            sql = f"SELECT id_venta, fecha, total, id_producto, id_usuario  FROM sale"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for item in result:
+                sale.append(Reports(item["fecha"], item["total"], item["id_producto"],item["id_usuario"],item["id_venta"]))
+            return sale
+    
+    @staticmethod
+    def count_all():
+        with mydb.cursor() as cursor:
+            sql = f"SELECT COUNT(id_venta) FROM sale"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            return result[0]
+        
+    def __str__(self):
+        return f"{ self.id_venta } - { self.fecha } - { self.total } - { self.id_producto } - { self.id_usuario } "
--- app/views/report_views.py
+++ app/views/report_views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
+from models.reports import Reports
 
 report_views = Blueprint('report',__name__)
 
 @report_views .route("/reportes/")
 def reports ():
-    return render_template('reports/reports.html')
+    reports = Reports.get_all()
+    return render_template('reports/reports.html',
+                           reports= reports)
